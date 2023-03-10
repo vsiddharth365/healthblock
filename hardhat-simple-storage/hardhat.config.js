8a8da9251e0295a3dd28752373a04f0b345f22c9
vsiddharth <vsiddharth365@gmail.com>
2023-03-10 21:37:40 +0530

    Added work done

---
+++ hardhat-simple-storage/hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "http://127.0.0.1:8545"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || process.env.PRIVATE_KEY_2
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            // accounts: already placed by hardhat
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.7",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+}
